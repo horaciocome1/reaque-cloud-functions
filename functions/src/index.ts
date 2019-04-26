806d7fdeaf16fb5ff6c54afb921ecc1d96f25684
Horácio Comé <horaciojr001@gmail.com>
2019-04-26 19:28:24 +0200

    partial fix [dont work either]

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,11 +7,10 @@ exports.createNotification = functions.firestore.document('posts/{postId}').onCr
     console.log('new post detected | ' + context.params.postId)
     const post = snap.data()
     if (post) {
-        console.log('initialize app | ' + context.params.postId)
         const db = admin.firestore()
 
         const data = {
-            message: post.user.name + ' tem uma nova publicação. ' + post.title + '.',
+            message: `${post.user.name} tem uma nova publicação. ${post.title}.`,
             pic: post.pic,
             date: context.timestamp,
             content_id: context.params.postId,
@@ -20,22 +19,28 @@ exports.createNotification = functions.firestore.document('posts/{postId}').onCr
         
         try {
             const ref = await db.collection('notifications').add(data)
-            console.log('notification added | ' + ref.id)
+            console.log(`notification added | ${ref.id}`)
 
-            const snapshot = await db.collection('users').where('favorites.' + post.user.id, '==', true).get()
-            console.log('users detected | ' + post.user.id)
+            // const snapshot = await db.collection('users').where(`favorites.${post.user.id}`, '==', true).get()
+            const snapshot = await db.doc(`users/${post.user.id}`).get()
+            const userData = snapshot.data()
+            if (userData) {
+                const favoriteFor = userData.favorite_for
+                console.log('users detected | ' + post.user.id)
 
-            const promises: Promise<FirebaseFirestore.WriteResult>[] = []
-            snapshot.forEach(doc => {
-                const users = new Map()
-                users.set(doc.id, true)
-                const promise = db.collection('notifications').doc(ref.id).set(data, {merge: true})
-                promises.push(promise)
-            });
-            await Promise.all(promises)
-            console.log('notification updated with users | ' + promises.length)
-        } catch (error) {
-            console.error('failed to write notification | ' + context.params.postId, error)
+                const promises: Promise<FirebaseFirestore.WriteResult>[] = []
+                for (const user in favoriteFor) {
+                    const users: FirebaseFirestore.DocumentData = new Map().set('users', new Map().set(user, true))
+                    const promise = db.doc(`notifications/${ref.id}`).set(users, {merge: true})
+                    promises.push(promise)
+                }
+                console.log(`total users | ${promises.length}`)
+
+                await Promise.all(promises)
+                console.log(`notification updated with users | ${promises.length}`)
+            }            
+        } catch (err) {
+            console.error(`failed to write notification ${context.params.postId}`, err)
         }
     }
 })
