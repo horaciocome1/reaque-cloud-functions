bd6c1ba450a96574a5912dcfdef3ac10c8a953e5
Horácio Comé <horaciojr001@gmail.com>
2019-04-29 12:10:28 +0200

    handling profile updates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,9 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp()
 
-exports.createNotification = functions.firestore.document('posts/{postId}').onCreate(async (snap, context) => {
+exports.onPostCreate = functions.firestore.document('posts/{postId}').onCreate(async (snap, context) => {
     console.log('new post detected | ' + context.params.postId)
-    
+
     const post = snap.data()
     if (post) {
         const db = admin.firestore()
@@ -34,7 +34,65 @@ exports.createNotification = functions.firestore.document('posts/{postId}').onCr
                         'users': {}
                     }
                     notification.users[user] = true
-                    const promise = db.doc(`notifications/${ref.id}`).set(notification, {merge: true})
+                    const promise = ref.set(notification, {merge: true})
+                    promises.push(promise)
+                }
+                console.log(`total users | ${promises.length}`)
+
+                await Promise.all(promises)
+                console.log(`notification updated with users | ${promises.length}`)
+            }            
+        } catch (err) {
+            console.error(`failed to write notification ${context.params.postId}`, err)
+        }
+    }
+})
+
+exports.onUserUpdatet = functions.firestore.document('users/{userId}').onUpdate(async (snap, context) => {
+    console.log('user update detected | ' + context.params.userId)
+
+    const before = snap.before.data()
+    const after = snap.after.data()
+
+    if (before && after) {
+        const db = admin.firestore()
+
+        var message = ''
+        if (before.bio == after.bio) {
+            message = `${before.name} actualizou o seu endereço. De ${before.address} para ${after.address}`
+            console.log('address update')
+        } else if (before.address == after.address) {
+            message = `${before.name} actualizou sua bio para "${after.bio}"`
+            console.log('bio update')
+        } else {
+            message = `${before.name} actualizou o seu perfil`
+            console.log('both address and bio update')
+        }
+
+        const data = {
+            message: message,
+            date: context.timestamp,
+            content_id: context.params.userId,
+            user: true
+        }
+        
+        try {
+            const ref = await db.collection('notifications').add(data)
+            console.log(`notification added | ${ref.id}`)
+
+            const snapshot = await db.doc(`users/${context.params.userId}`).get()
+            const userData = snapshot.data()
+            if (userData) {
+                const favoriteFor = userData.favorite_for
+                console.log('users detected | ' + context.params.userId)
+
+                const promises: Promise<FirebaseFirestore.WriteResult>[] = []
+                for (const user2 in favoriteFor) {
+                    const notification: FirebaseFirestore.DocumentData = {
+                        'users': {}
+                    }
+                    notification.users[user2] = true
+                    const promise = ref.set(notification, {merge: true})
                     promises.push(promise)
                 }
                 console.log(`total users | ${promises.length}`)
