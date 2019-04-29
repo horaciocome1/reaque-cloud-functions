ef71d6f60b270e5bebfa36ec622c90a669d8db6d
Horácio Comé <horaciojr001@gmail.com>
2019-04-29 13:38:31 +0200

    followers count

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -48,7 +48,7 @@ exports.onPostCreate = functions.firestore.document('posts/{postId}').onCreate(a
     }
 })
 
-exports.onUserUpdatet = functions.firestore.document('users/{userId}').onUpdate(async (snap, context) => {
+exports.onUserUpdate = functions.firestore.document('users/{userId}').onUpdate(async (snap, context) => {
     console.log('user update detected | ' + context.params.userId)
 
     const before = snap.before.data()
@@ -57,51 +57,69 @@ exports.onUserUpdatet = functions.firestore.document('users/{userId}').onUpdate(
     if (before && after) {
         const db = admin.firestore()
 
-        var message = ''
-        if (before.bio == after.bio) {
-            message = `${before.name} actualizou o seu endereço. De ${before.address} para ${after.address}`
-            console.log('address update')
-        } else if (before.address == after.address) {
-            message = `${before.name} actualizou sua bio para "${after.bio}"`
-            console.log('bio update')
-        } else {
-            message = `${before.name} actualizou o seu perfil`
-            console.log('both address and bio update')
-        }
+        if (before.favorite_for !== after.favorite_for) {
+            console.log('somebody started or stopped following')
 
-        const data = {
-            message: message,
-            date: context.timestamp,
-            content_id: context.params.userId,
-            user: true
-        }
-        
-        try {
-            const ref = await db.collection('notifications').add(data)
-            console.log(`notification added | ${ref.id}`)
-
-            const snapshot = await db.doc(`users/${context.params.userId}`).get()
-            const userData = snapshot.data()
-            if (userData) {
-                const favoriteFor = userData.favorite_for
-                console.log('users detected | ' + context.params.userId)
-
-                const promises: Promise<FirebaseFirestore.WriteResult>[] = []
-                for (const user2 in favoriteFor) {
-                    const notification: FirebaseFirestore.DocumentData = {
-                        'users': {}
-                    }
-                    notification.users[user2] = true
-                    const promise = ref.set(notification, {merge: true})
-                    promises.push(promise)
+            try {
+                let count = 0
+                for (const user in after.favorite_for) {
+                    if (after.favorite_for[user] === true)
+                        count++
                 }
-                console.log(`total users | ${promises.length}`)
+                
+                await db.doc(`users/${context.params.userId}`).set({followers_count: count}, {merge: true})
+                console.log(`followers count update | ${count}`)
+            } catch (err) {
+                console.log('followers update failed')
+            }
+        } else if (before.bio !== after.bio || before.address !== after.address) {
+            let message = ''
 
-                await Promise.all(promises)
-                console.log(`notification updated with users | ${promises.length}`)
-            }            
-        } catch (err) {
-            console.error(`failed to write notification ${context.params.postId}`, err)
+            if (before.bio === after.bio) {
+                message = `${before.name} actualizou o seu endereço. De ${before.address} para ${after.address}`
+                console.log('address update')
+            } else if (before.address === after.address) {
+                message = `${before.name} actualizou sua bio para "${after.bio}"`
+                console.log('bio update')
+            } else {
+                message = `${before.name} actualizou o seu perfil`
+                console.log('both address and bio update')
+            }
+
+            const data = {
+                message: message,
+                date: context.timestamp,
+                content_id: context.params.userId,
+                user: true
+            }
+            
+            try {
+                const ref = await db.collection('notifications').add(data)
+                console.log(`notification added | ${ref.id}`)
+
+                const snapshot = await db.doc(`users/${context.params.userId}`).get()
+                const userData = snapshot.data()
+                if (userData) {
+                    const favoriteFor = userData.favorite_for
+                    console.log('users detected | ' + context.params.userId)
+
+                    const promises: Promise<FirebaseFirestore.WriteResult>[] = []
+                    for (const user2 in favoriteFor) {
+                        const notification: FirebaseFirestore.DocumentData = {
+                            'users': {}
+                        }
+                        notification.users[user2] = true
+                        const promise = ref.set(notification, {merge: true})
+                        promises.push(promise)
+                    }
+                    console.log(`total users | ${promises.length}`)
+
+                    await Promise.all(promises)
+                    console.log(`notification updated with users | ${promises.length}`)
+                }            
+            } catch (err) {
+                console.error(`failed to write notification ${context.params.postId}`, err)
+            }
         }
     }
 })
