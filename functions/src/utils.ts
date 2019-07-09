650cb675399fe247aeb03142cf2cca7128ba808d
Horácio Comé <horaciojr001@gmail.com>
2019-07-09 14:57:32 +0200

    fixes

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-import { Timestamp} from '@google-cloud/firestore'
+import { Timestamp } from '@google-cloud/firestore'
 
 const db = admin.firestore()
 
