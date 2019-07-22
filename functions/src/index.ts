71d908f54ebae5b982c6313cfbab0b96b5c25f04
Horácio Comé <horaciojr001@gmail.com>
2019-07-22 16:18:43 +0200

    succeed to create functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -35,3 +35,39 @@ export const onPostCreated = functions.firestore.document('posts/{postId}').onCr
 export const onAccountCreated = functions.auth.user().onCreate(
     async (user, _) => await utils.initializeUser(user)
 )
+
+export const updatingEachPostScore = functions.https.onRequest(
+    async (_, res) => {
+        const hasErros = await utils.handlingUpdatingEachPostScore()
+        let message = ''
+        if (hasErros === true)
+            message = 'Successful!'
+        else
+            message = 'We had some errors! Please visit the logs'
+        res.send(message)
+    }
+)
+
+export const updatingEachTopicScore = functions.https.onRequest(
+    async (_, res) => {
+        const hasErros = await utils.handlingUpdatingEachTopicScore()
+        let message = ''
+        if (hasErros === true)
+            message = 'Successful!'
+        else
+            message = 'We had some errors! Please visit the logs'
+        res.send(message)
+    }
+)
+
+export const updatingEachUserScore = functions.https.onRequest(
+    async (_, res) => {
+        const hasErros = await utils.handlingUpdatingEachUserScore()
+        let message = ''
+        if (hasErros === true)
+            message = 'Successful!'
+        else
+            message = 'We had some errors! Please visit the logs'
+        res.send(message)
+    }
+)
